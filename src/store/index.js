777719733f5f1419cd855a19dde5ce5586ee4270
Ammar Syed <asyed@ufl.edu>
2020-08-29 14:29:45 -0400

    some stuff

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
-
+import Vue from "vue";
+import Vuex from "vuex";
+Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
+    images: []
   },
   mutations: {
+    setImages(state, payload)
+    {
+      state.images = payload;
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {}
+});
